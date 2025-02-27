2e2587c2d97f346942e559dcf6edab37d79bf3f5
Nicoli Lisboa <nick.lisboa04@gmail.com>
2025-02-27 00:14:46 -0300

    WIP: Novo

--- src/js/header.js
+++ src/js/header.js
@@ -13,15 +13,4 @@ document.addEventListener("DOMContentLoaded", function() {
             textElement.appendChild(span);
         });
     });
-
-    // Função de rolagem para header
-    const header = document.querySelector('.header_opcoes');
-
-    window.addEventListener("scroll", function() {
-        if (window.scrollY > 10) {
-            header.classList.add('shrink'); 
-        } else {
-            header.classList.remove('shrink'); 
-        }
-    });
 });
