d78a178c5c328b03948b6b0f8e554afbad120dba
Nicoli Lisboa <nick.lisboa04@gmail.com>
2025-07-20 20:46:07 -0300

    .

--- src/js/project.js
+++
@@ -1,9 +0,0 @@
-function scrollProjects(direction) {
-    const scrollContainer = document.querySelector('.project-wrapper .project');
-    const scrollAmount = 340;
-
-    scrollContainer.scrollBy({
-      left: scrollAmount * direction,
-      behavior: 'smooth'
-    });
-}
